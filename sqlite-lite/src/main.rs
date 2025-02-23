9db597d0e0a6ef1ebdc0e6f8c2acb55448dde092
Brendan Dagys <brendandagys@gmail.com>
2025-02-22 21:11:17 -0500

    Implement basic CLI with support for `.exit` command

--- sqlite-lite/src/main.rs
+++ sqlite-lite/src/main.rs
@@ -1,3 +1,42 @@
+use std::io::{self, Write};
+
+struct InputBuffer {
+    buffer: String,
+}
+
+impl InputBuffer {
+    fn new() -> Self {
+        Self {
+            buffer: String::new(),
+        }
+    }
+
+    fn read_input(&mut self) {
+        self.buffer.clear();
+        io::stdin()
+            .read_line(&mut self.buffer)
+            .expect("Failed to read line");
+        self.buffer = self.buffer.trim_end().to_string(); // Remove trailing `\n`
+    }
+}
+
+fn print_prompt() {
+    print!("sqlite-lite> ");
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_buffer = InputBuffer::new();
+
+    loop {
+        print_prompt();
+        input_buffer.read_input();
+
+        if input_buffer.buffer == ".exit" {
+            std::process::exit(0);
+        } else {
+            println!("Unrecognized command '{}'.", input_buffer.buffer);
+            io::stdout().flush().unwrap();
+        }
+    }
 }
